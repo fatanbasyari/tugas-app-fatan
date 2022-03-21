f001e3ea29c2290e9659d43e5486f47abac1a1c9
Fatan <fatanbasyari82@gmail.com>
2022-03-21 14:04:52 +0700

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+    return (
+        <body>
+            <div className='kotak1'>
+                <h1 className='judul'>hello</h1>
+                <input className='inputan' type={'text'} placeholder='password'></input>
+                <button clasName='tombol'>login</button>
+            </div>
+        </body>
+    );
+}
+
+export default App;
